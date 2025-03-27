320d4fd4f2a5f742731f429487dbc5e44a06ceab
rasmine <blasrasmine@gmail.com>
2025-03-27 15:08:29 +0800

    Returns the string with capitalized and the rest in lowercase

--- capitalizes_the_first_letter_of_a_string_converts_all_letters_to_lowercase_without_using_capitalize().py
+++ capitalizes_the_first_letter_of_a_string_converts_all_letters_to_lowercase_without_using_capitalize().py
@@ -1,4 +1,12 @@
 # capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
 
 # Returns the string with the first letter capitalized and the rest in lowercase
+def custom_capitalize(s):
+    if not s:
+        return s 
+    
+    first_char = s[0]  
+    remaining_chars = s[1:]  
+    return (chr(ord(first_char) - 32) if 'a' <= first_char <= 'z' else first_char) + remaining_chars.lower()
+
 # Get user input and prints result

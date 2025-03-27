bf50932e38824d8b06d777578928879b1d5aa9a1
rasmine <blasrasmine@gmail.com>
2025-03-27 14:44:21 +0800

    Pseudocode

---
+++ replicates_the_functionality_of_ljust()_without_using_ljust().py
@@ -0,0 +1,4 @@
+# ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+
+# Returns the string with spaces added at the end to match the specified width
+# Get user input and prints result

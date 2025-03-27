25b58e12ce3985e7d8b39a8c54653350520a3b4f
rasmine <blasrasmine@gmail.com>
2025-03-27 14:06:30 +0800

    Define function

--- removes_leading_spaces_without_using_lstrip().py
+++ removes_leading_spaces_without_using_lstrip().py
@@ -1,4 +1,9 @@
 # lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 
 # Define a function named custom_lstrip that takes a string 's' as input
+def custom_lstrip(s):
+    i = 0
+    while i < len(s) and s[i] == ' ':
+        i += 1
+    return s[i:]
 # Get user input and print output

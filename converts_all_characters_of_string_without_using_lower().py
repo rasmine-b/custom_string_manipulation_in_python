94022c9780d301064a37a0a5c28ac4d1e1ade67d
rasmine <blasrasmine@gmail.com>
2025-03-27 14:26:18 +0800

    Pseudocode

---
+++ converts_all_characters_of_string_without_using_lower().py
@@ -0,0 +1,4 @@
+# lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
+# Convert uppercase letters (A-Z) to lowercase (a-z) manually
+# Get user input and prints result

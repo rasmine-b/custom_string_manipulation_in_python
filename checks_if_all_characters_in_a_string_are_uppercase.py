50ff4eb2137fd5de1efdeec2bdd0044994678a6e
rasmine <blasrasmine@gmail.com>
2025-03-27 14:34:51 +0800

    Checks and returns True or False

--- checks_if_all_characters_in_a_string_are_uppercase.py
+++ checks_if_all_characters_in_a_string_are_uppercase.py
@@ -1,4 +1,10 @@
 # isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
 
 # Checks and returns True if all letters in the string are uppercase; otherwise, returns False
+def custom_isupper(s):
+    for char in s:
+        if 'a' <= char <= 'z': 
+            return False
+    return True
+
 # Get user input and prints result

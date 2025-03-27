d56c4b4103f69154640acd6da9a657e3fe5f2dbd
rasmine <blasrasmine@gmail.com>
2025-03-27 14:45:21 +0800

    Returns the string to match the specified width

--- replicates_the_functionality_of_ljust()_without_using_ljust().py
+++ replicates_the_functionality_of_ljust()_without_using_ljust().py
@@ -1,4 +1,7 @@
 # ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
 
 # Returns the string with spaces added at the end to match the specified width
+def custom_ljust(s, width):
+    return s + ' ' * (width - len(s)) if len(s) < width else s
+
 # Get user input and prints result

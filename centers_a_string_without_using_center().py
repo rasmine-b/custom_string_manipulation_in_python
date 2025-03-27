98973e8bc8fbaed0f0e5616f287bc9869dcef78c
rasmine <blasrasmine@gmail.com>
2025-03-27 14:52:30 +0800

    Pseudocode

---
+++ centers_a_string_without_using_center().py
@@ -0,0 +1,4 @@
+# center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+
+# Returns the string centered within the given width by adding spaces on both sides
+# Get user input and prints result

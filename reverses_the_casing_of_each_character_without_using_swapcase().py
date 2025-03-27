7e4fd202538a167d1ff1a2716be3247f8fddfbab
rasmine <blasrasmine@gmail.com>
2025-03-27 15:00:34 +0800

    Pseudocode

---
+++ reverses_the_casing_of_each_character_without_using_swapcase().py
@@ -0,0 +1,4 @@
+# swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+
+# Returns a new string where uppercase letters become lowercase and vice versa
+# Get user input and prints result

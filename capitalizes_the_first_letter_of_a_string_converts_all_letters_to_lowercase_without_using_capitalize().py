b0d6f2770bd923d5ea6cfcc57b89c0a113577408
rasmine <blasrasmine@gmail.com>
2025-03-27 15:06:07 +0800

    Pseudocode

---
+++ capitalizes_the_first_letter_of_a_string_converts_all_letters_to_lowercase_without_using_capitalize().py
@@ -0,0 +1,4 @@
+# capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
+
+# Returns the string with the first letter capitalized and the rest in lowercase
+# Get user input and prints result

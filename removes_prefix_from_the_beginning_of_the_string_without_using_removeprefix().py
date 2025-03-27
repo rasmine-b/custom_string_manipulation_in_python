984541875e29042f86fd6d369d576da7dc8f7596
rasmine <blasrasmine@gmail.com>
2025-03-27 14:16:15 +0800

    Pseudocode

---
+++ removes_prefix_from_the_beginning_of_the_string_without_using_removeprefix().py
@@ -0,0 +1,4 @@
+# removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+
+# If the string starts with the given prefix, remove it; otherwise, return the original string
+# Get user input and prints result

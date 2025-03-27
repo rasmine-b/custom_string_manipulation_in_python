5ba70c6765f5c1c574347865663a6f35a6ca2181
rasmine <blasrasmine@gmail.com>
2025-03-27 14:22:00 +0800

    removes prefix without using removeprefix()

--- removes_prefix_from_the_beginning_of_the_string_without_using_removeprefix().py
+++ removes_prefix_from_the_beginning_of_the_string_without_using_removeprefix().py
@@ -1,4 +1,8 @@
 # removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
 
 # If the string starts with the given prefix, remove it; otherwise, return the original string
+def custom_removeprefix(s, prefix):
+    if s[:len(prefix)] == prefix:
+        return s[len(prefix):]
+    return s 
 # Get user input and prints result

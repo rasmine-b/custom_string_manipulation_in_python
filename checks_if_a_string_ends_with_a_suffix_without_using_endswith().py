281e1ba9b6042ab194a02125a8325565c89f995a
rasmine <blasrasmine@gmail.com>
2025-03-27 14:40:52 +0800

    Checks and Returns True or False

--- checks_if_a_string_ends_with_a_suffix_without_using_endswith().py
+++ checks_if_a_string_ends_with_a_suffix_without_using_endswith().py
@@ -1,4 +1,7 @@
 # endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 
 # Checks and returns True if the string ends with the given suffix; otherwise, returns False
+def custom_endswith(s, suffix):
+    return s[-len(suffix):] == suffix if len(suffix) <= len(s) else False
+
 # Get user input and prints result

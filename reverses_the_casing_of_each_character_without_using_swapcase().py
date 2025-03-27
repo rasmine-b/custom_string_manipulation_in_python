fd21cd0c10774b09e431311fb1e6b00379401178
rasmine <blasrasmine@gmail.com>
2025-03-27 15:02:04 +0800

    Return a new string where the letters swap cases

--- reverses_the_casing_of_each_character_without_using_swapcase().py
+++ reverses_the_casing_of_each_character_without_using_swapcase().py
@@ -1,4 +1,15 @@
 # swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
 
 # Returns a new string where uppercase letters become lowercase and vice versa
+def custom_swapcase(s):
+    result = ""
+    for char in s:
+        if 'A' <= char <= 'Z':  
+            result += chr(ord(char) + 32)  
+        elif 'a' <= char <= 'z':  
+            result += chr(ord(char) - 32)  
+        else:
+            result += char 
+    return result
+
 # Get user input and prints result

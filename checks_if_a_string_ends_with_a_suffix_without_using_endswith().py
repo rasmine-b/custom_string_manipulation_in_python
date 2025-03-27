cddfbe4a1110cba5e593675b99074be0ca10b511
rasmine <blasrasmine@gmail.com>
2025-03-27 14:39:47 +0800

    Pseudocode

---
+++ checks_if_a_string_ends_with_a_suffix_without_using_endswith().py
@@ -0,0 +1,4 @@
+# endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+
+# Checks and returns True if the string ends with the given suffix; otherwise, returns False
+# Get user input and prints result

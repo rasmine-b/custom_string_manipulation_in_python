1b3a67a59a82ce8139ade396d1754f4cd3b7f0d6
rasmine <blasrasmine@gmail.com>
2025-03-29 15:25:34 +0800

    Get user input and prints result

--- replicates_the_functionality_of_rjust()_without_using_rjust().py
+++ replicates_the_functionality_of_rjust()_without_using_rjust().py
@@ -5,3 +5,6 @@ def custom_rjust(s, width):
     return ' ' * (width - len(s)) + s if len(s) < width else s
 
 # Get user input and prints result
+text = input("Enter a string: ")
+width = int(input("Enter the total width: "))
+print(f"Modified: '{custom_rjust(text, width)}'")

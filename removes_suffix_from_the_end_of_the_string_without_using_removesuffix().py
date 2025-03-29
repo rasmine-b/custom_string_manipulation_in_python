d164f6b4e9efa282f48f60d11f70b443542a4e14
rasmine <blasrasmine@gmail.com>
2025-03-29 14:54:02 +0800

    Pseudocode

---
+++ removes_suffix_from_the_end_of_the_string_without_using_removesuffix().py
@@ -0,0 +1,4 @@
+# removesuffix(). remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+
+# If the string starts with the given suffix, remove it; otherwise, return the original string
+# Get user input and prints result

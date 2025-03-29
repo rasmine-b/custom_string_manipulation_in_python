79a9d87da53504fb873d0ab1ffa35b015642f0ce
rasmine <blasrasmine@gmail.com>
2025-03-29 14:59:53 +0800

    Pseudocode

---
+++ converts_all_characters_of_string_without_using_upper().py
@@ -0,0 +1,5 @@
+# upper(). converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+
+# Convert lowercase letters (a-z) to uppercase (A-Z) manually
+# Get user input and prints result
+

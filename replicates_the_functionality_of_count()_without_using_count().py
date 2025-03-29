236d36b1963dc45e50df477c51574907af782a71
rasmine <blasrasmine@gmail.com>
2025-03-29 15:45:01 +0800

    Function that counts

--- replicates_the_functionality_of_count()_without_using_count().py
+++ replicates_the_functionality_of_count()_without_using_count().py
@@ -1,4 +1,7 @@
 # count(). return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 
 # Function to count how many times a substring appears in a string
+def custom_count(s, sub):
+    return sum(1 for i in range(len(s) - len(sub) + 1) if s[i:i+len(sub)] == sub)
+
 # Get user input and prints result

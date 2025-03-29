4f27f8e87ab1de7f9a8c06f57d14d0d37f81b2f9
rasmine <blasrasmine@gmail.com>
2025-03-29 15:52:16 +0800

    Pseudocode

---
+++ replicates_the_functionality_of_rindex()_without_using_rindex().py
@@ -0,0 +1,4 @@
+# rindex(). return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
+
+# Custom function to find the last occurrence of a character in a string
+# Get user input and prints result

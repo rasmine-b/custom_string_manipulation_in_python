542ebf7c46fc435b799b1b47104d845db6ee5bd4
rasmine <blasrasmine@gmail.com>
2025-03-29 15:08:40 +0800

    Checks and return True or False

--- checks_if_all_characters_in_a_string_are_lowercase.py
+++ checks_if_all_characters_in_a_string_are_lowercase.py
@@ -1,4 +1,10 @@
 # islower(). check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
 
 # Checks and returns True if all letters in the string are lowercase; otherwise, returns False
+def custom_islower(s):
+    for char in s:
+        if 'A' <= char <= 'Z':
+            return False
+    return True
+
 # Get user input and prints result

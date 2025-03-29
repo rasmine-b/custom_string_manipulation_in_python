ee886963d3545fcc2240cbc480394ffd1512ef3b
rasmine <blasrasmine@gmail.com>
2025-03-29 15:39:33 +0800

    Pseudocode

---
+++ replicates_the_functionality_of_count()_without_using_count().py
@@ -0,0 +1,4 @@
+# count(). return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+
+# Function to count how many times a substring appears in a string
+# Get user input and prints result

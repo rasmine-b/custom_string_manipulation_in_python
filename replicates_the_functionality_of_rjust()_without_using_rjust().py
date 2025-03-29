288eacda6f8446fd7c1a4908668f8e5474c96c1b
rasmine <blasrasmine@gmail.com>
2025-03-29 15:24:55 +0800

    Returns the string with spaces

--- replicates_the_functionality_of_rjust()_without_using_rjust().py
+++ replicates_the_functionality_of_rjust()_without_using_rjust().py
@@ -1,4 +1,7 @@
 # rjust(). add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
 
 # Returns the string with spaces added at the end to match the specified width
+def custom_rjust(s, width):
+    return ' ' * (width - len(s)) + s if len(s) < width else s
+
 # Get user input and prints result

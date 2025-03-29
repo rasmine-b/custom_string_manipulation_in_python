f0fd3105bfae0ad6f9109e7dafbf7764d1eb4626
rasmine <blasrasmine@gmail.com>
2025-03-29 15:54:22 +0800

    Updated commit message

--- replicates_the_functionality_of_rindex()_without_using_rindex().py
+++ replicates_the_functionality_of_rindex()_without_using_rindex().py
@@ -1,10 +1,11 @@
 # rindex(). return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
 
 # Custom function to find the last occurrence of a character in a string
-def rindex(s, char):
+def custom_rindex(s, char):
     for i in range(len(s) - 1, -1, -1):
         if s[i] == char:
             return i
     raise ValueError(f"'{char}' not found in string")
 
 # Get user input and prints result
+

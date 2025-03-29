6668d7f048d83a25e4093e3d64dc7c0e7e98f5d9
rasmine <blasrasmine@gmail.com>
2025-03-29 14:39:13 +0800

    Pseudocode

---
+++ removes_leading_spaces_without_using_rstrip().py
@@ -0,0 +1,4 @@
+# rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+
+# Define a function named custom_rstrip that takes a string 's' as input
+# Get user input and prints result

dba38080ddf38dbf262ef42da22da624fdf3d191
rasmine <blasrasmine@gmail.com>
2025-03-29 15:23:42 +0800

    Pseudocode

---
+++ replicates_the_functionality_of_rjust()_without_using_rjust().py
@@ -0,0 +1,4 @@
+# rjust(). add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+
+# Returns the string with spaces added at the end to match the specified width
+# Get user input and prints result

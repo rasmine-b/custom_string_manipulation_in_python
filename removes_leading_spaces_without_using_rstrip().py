74f7c488f7b65b062f9f70e07c140b5bbd333356
rasmine <blasrasmine@gmail.com>
2025-03-29 14:47:40 +0800

    Define a function named custom_rstrip()

--- removes_leading_spaces_without_using_rstrip().py
+++ removes_leading_spaces_without_using_rstrip().py
@@ -1,4 +1,10 @@
 # rstrip(). remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
 
 # Define a function named custom_rstrip that takes a string 's' as input
+def custom_rstrip(s):
+    i = len(s) - 1
+    while i >= 0 and s[i] == ' ':
+        i -= 1
+    return s[:i + 1]
+
 # Get user input and prints result

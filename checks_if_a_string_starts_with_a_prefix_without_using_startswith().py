812dba4a90124e8320caa1bca3a5996231a76484
rasmine <blasrasmine@gmail.com>
2025-03-29 15:12:52 +0800

    Pseudocode

---
+++ checks_if_a_string_starts_with_a_prefix_without_using_startswith().py
@@ -0,0 +1,4 @@
+# startswith(). check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+
+# Checks and returns True if the string ends with the given prefix; otherwise, returns False
+# Get user input and prints result

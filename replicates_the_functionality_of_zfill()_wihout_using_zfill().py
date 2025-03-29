0495b912c32f75c6fc5b7f6eaec04b944e9cdf88
rasmine <blasrasmine@gmail.com>
2025-03-29 15:34:46 +0800

    Custom function to add leading zeros

--- replicates_the_functionality_of_zfill()_wihout_using_zfill().py
+++ replicates_the_functionality_of_zfill()_wihout_using_zfill().py
@@ -1,4 +1,7 @@
 # zfill(). add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 
 # Custom function to add leading zeros to a string
+def custom_zfill(s, width):
+    return '0' * (width - len(s)) + s if len(s) < width else s
+
 # Get user input and prints result

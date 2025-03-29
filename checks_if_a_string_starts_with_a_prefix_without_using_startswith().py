de07d2ead77f7f3d8cb9e617ed4e8ee8327cbd59
rasmine <blasrasmine@gmail.com>
2025-03-29 15:17:56 +0800

    Checks and returns True or False

--- checks_if_a_string_starts_with_a_prefix_without_using_startswith().py
+++ checks_if_a_string_starts_with_a_prefix_without_using_startswith().py
@@ -1,4 +1,7 @@
 # startswith(). check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
 
 # Checks and returns True if the string ends with the given prefix; otherwise, returns False
+def custom_startswith(s, prefix):
+    return s[:len(prefix)] == prefix if len(prefix) <= len(s) else False
+
 # Get user input and prints result

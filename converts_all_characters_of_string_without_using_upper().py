ef7dd3b094293a15c3177c6c0f4023e260edac3b
rasmine <blasrasmine@gmail.com>
2025-03-29 15:01:01 +0800

    Convert lowercase to uppercase

--- converts_all_characters_of_string_without_using_upper().py
+++ converts_all_characters_of_string_without_using_upper().py
@@ -1,5 +1,14 @@
 # upper(). converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 
 # Convert lowercase letters (a-z) to uppercase (A-Z) manually
+def custom_upper(s):
+    result = ""
+    for char in s:
+        if 'a' <= char <= 'z':
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    return result
+
 # Get user input and prints result
 
